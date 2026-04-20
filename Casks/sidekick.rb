5bd2850eb944653c787c4eed20560440d07c934d
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-04-20 15:57:55 -0400

    Fix issues in Sidekick cask

--- Casks/sidekick.rb
+++ Casks/sidekick.rb
@@ -2,8 +2,8 @@ cask "sidekick" do
   version "1.0.0-rc.18"
   sha256 "1db652cca6617a6da500b96a5841d5c67c22c77637e7203543cb5872183eb794"
 
-  url "https://github.com/johnbean393/Sidekick/releases/download/#{version}/Sidekick.#{version}.dmg"
-  name "sidekick"
+  url "https://github.com/johnbean393/Sidekick/releases/download/#{version}/Sidekick.#{version}.dmg", verified "github.com/johnbean393/Sidekick/"
+  name "Sidekick"
   desc "A native macOS app that allows users to chat with a local LLM that can respond with information from files, folders and websites on your Mac without installing any other software. Powered by llama.cpp."
   homepage "https://github.com/johnbean393/Sidekick"
 
@@ -11,6 +11,7 @@ cask "sidekick" do
 
   livecheck do
     url :url
-    strategy :github_latest
+    strategy :github_releases
+    regex(/^v?(\d+(?:\.\d+)+(?:-[a-z]+[.-]?\d+)?)$/i)
   end
 end
