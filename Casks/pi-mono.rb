86be26a79adb3d49a9a86145a7fae582132f1d5d
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-04-26 10:42:04 -0400

    Fix pi workflow

--- Casks/pi-mono.rb
+++ Casks/pi-mono.rb
@@ -1,13 +1,19 @@
 cask "pi-mono" do
-  arch arm: "arm64", intel: "x64"
-
   version "0.70.2"
-  sha256 arm:   "4e039050367d82bff963e15fea8d85ca268f7b0284b83f5f15939aad087353a4",
-         intel: "0ffed2128efb469ef23c46f861cd1d848022153ba7247dab3ee7ea41bf6c3864"
 
-  url "https://github.com/badlogic/pi-mono/releases/download/v#{version}/pi-darwin-#{arch}.tar.gz"
+  on_arm do
+    sha256 "4e039050367d82bff963e15fea8d85ca268f7b0284b83f5f15939aad087353a4"
+
+    url "https://github.com/badlogic/pi-mono/releases/download/v#{version}/pi-darwin-arm64.tar.gz"
+  end
+  on_intel do
+    sha256 "0ffed2128efb469ef23c46f861cd1d848022153ba7247dab3ee7ea41bf6c3864"
+
+    url "https://github.com/badlogic/pi-mono/releases/download/v#{version}/pi-darwin-x64.tar.gz"
+  end
+
   name "Pi"
-  desc "AI agent toolkit: coding agent CLI, unified LLM API, TUI & web UI libraries, Slack bot, vLLM pods"
+  desc "AI agent toolkit with CLI, unified LLM API, TUI, and web UI libraries"
   homepage "https://github.com/badlogic/pi-mono"
 
   livecheck do
