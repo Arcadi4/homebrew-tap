85673675e6eb652f186af789c09a634506ab26bd
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-09-01 22:54:32 -0400

    Fix sidekick-ai's update workflow

--- Casks/sidekick-ai.rb
+++ Casks/sidekick-ai.rb
@@ -1,8 +1,8 @@
 cask "sidekick-ai" do
   version "1.0.0-rc.18"
   sha256 "1db652cca6617a6da500b96a5841d5c67c22c77637e7203543cb5872183eb794"
-  url "https://github.com/johnbean393/Sidekick/releases/download/#{version}/Sidekick.#{version}.dmg",
-      verified: "github.com/johnbean393/Sidekick/"
+
+  url "https://github.com/johnbean393/Sidekick/releases/download/#{version}/Sidekick.#{version}.dmg"
   name "Sidekick"
   desc "Chat with a local LLM that can use information from files, folders and websites"
   homepage "https://github.com/johnbean393/Sidekick"
