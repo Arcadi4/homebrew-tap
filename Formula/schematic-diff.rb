e9edd247c9b0db8361ab58fea5ecee3564c590f1
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-09-18 22:19:11 -0400

    Bump schematic-diff to 0.3.0

--- Formula/schematic-diff.rb
+++ Formula/schematic-diff.rb
@@ -5,23 +5,23 @@ class SchematicDiff < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Arcadi4/schematic-diff/releases/download/v0.1.0/schematic-diff-aarch64-apple-darwin.tar.gz"
-      sha256 "d74564a2304b54ced37bdac7e8da34d91e51e34d260c67642be0baf2fa6c30ec"
+      url "https://github.com/Arcadi4/schematic-diff/releases/download/v0.3.0/schematic-diff-aarch64-apple-darwin.tar.gz"
+      sha256 "9dd47026b2c2f56583e41b92523ae28746c52400f86b768813b246b0aa7666ae"
     end
     on_intel do
-      url "https://github.com/Arcadi4/schematic-diff/releases/download/v0.1.0/schematic-diff-x86_64-apple-darwin.tar.gz"
-      sha256 "c1351862a27f93d5c6a35336d527897062fb2f7c0938b6cb63f110efb4e6a1c1"
+      url "https://github.com/Arcadi4/schematic-diff/releases/download/v0.3.0/schematic-diff-x86_64-apple-darwin.tar.gz"
+      sha256 "4a9d75740f90a83cba4101094f47d419afcce1c88ec9ae2208c0cc827c44622d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Arcadi4/schematic-diff/releases/download/v0.1.0/schematic-diff-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "fe9024037ad66eee4f56b4ffafd0ba075eea3f462fb104d71bd178e7b52823f8"
+      url "https://github.com/Arcadi4/schematic-diff/releases/download/v0.3.0/schematic-diff-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7d5c7b510c335e82f7dc20830f92c71a05588215924ede8134e7a88cef268360"
     end
     on_intel do
-      url "https://github.com/Arcadi4/schematic-diff/releases/download/v0.1.0/schematic-diff-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "42abb39d42b509dc1ea8395ad09450e7e5e965d71d1d10fadde39f3967e532de"
+      url "https://github.com/Arcadi4/schematic-diff/releases/download/v0.3.0/schematic-diff-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "7b8127992a4fc3ca8bb67539f283b495e74371f2a4e287b311a3db9c7ca2abbc"
     end
   end
 
