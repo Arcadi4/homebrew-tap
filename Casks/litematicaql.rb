71e313c211c1ae03be3400bb9fb6cdba75d6b7d6
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-09-19 10:30:56 -0400

    Bump litematicaql to 1.2.1

--- Casks/litematicaql.rb
+++ Casks/litematicaql.rb
@@ -1,9 +1,9 @@
 cask "litematicaql" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.1.0"
-  sha256 arm:   "ba0d2214caa672d9e6912d8ea497c751d7a64b894e8906e43003e90e339e7c35",
-         intel: "543a8ce70ce04648ecc384da0133d6eddd333c7036d386c3da02492561bf2e02"
+  version "1.2.1"
+  sha256 arm:   "6864243eb23c8ffda8f86e659ab5ecc4d21c1c0ac5f08c45c0662e6cc9fee6e8",
+         intel: "8006d0954b4a8903ba27a89acc0f7bb631fb000304f6308e04d76081b01fd0ee"
 
   url "https://github.com/Arcadi4/LitematicaQL/releases/download/v#{version}/LitematicaQL-v#{version}-#{arch}.zip"
   name "LitematicaQL"
